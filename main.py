d7c737f29d025081698d1d4bb0e27668ee6e5b19
Hagjoon <105399835+leehagjoon@users.noreply.github.com>
2024-03-11 12:51:00 +0900

    수정,삭제 추가

--- main.py
+++ main.py
@@ -18,4 +18,19 @@ def create_memo(memo:Memo):
 def read_memo():
     return memos
 
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id == req_memo.id:
+            memo.content = req_memo.content
+            return '성공'
+    return '요청 실패'
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+    for index, memo in enumerate(memos):
+        if memo.id == memo.id:
+            memos.pop(index)
+            return '성공'
+    return '실패'
 app.mount("/", StaticFiles(directory="static",html=True), name="static")
